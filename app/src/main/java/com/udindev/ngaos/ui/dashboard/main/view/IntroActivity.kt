01acb23b30d3b841cacce57cf0002cd9bb625a31
nngarfdn <nanangarif404@gmail.com>
2021-06-28 10:26:45 +0700

    intent to dashboard

--- app/src/main/java/com/udindev/ngaos/ui/dashboard/main/view/IntroActivity.kt
+++ app/src/main/java/com/udindev/ngaos/ui/dashboard/main/view/IntroActivity.kt
@@ -1,10 +1,9 @@
 package com.udindev.ngaos.ui.dashboard.main.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.udindev.ngaos.MainActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.udindev.ngaos.R
 
 class IntroActivity : AppCompatActivity() {
@@ -15,7 +14,7 @@ class IntroActivity : AppCompatActivity() {
         var btn_intro = findViewById(R.id.btn_mulai) as Button
 
         btn_intro.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent);
         }
     }
